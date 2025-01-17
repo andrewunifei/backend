2cdaec2ce84f0a9802020dcbf70535772c010646
Andrew <andrewunifei@gmail.com>
2025-01-16 22:09:30 -0300

    [ADD] Configurações do docker-compose e Dockerfile

--- app/database/session.py
+++ app/database/session.py
@@ -3,6 +3,5 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 SQLITE_URL = os.getenv('SQLITE_URL')
-print(SQLITE_URL)
 engine = create_engine(SQLITE_URL)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
